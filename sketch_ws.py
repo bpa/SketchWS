08e4344251ae15d5a029a18c5dbb3b1a76742154
Bruce Armstrong <bruce@fortressofgeekdom.org>
2015-09-15 00:34:11 -0600

    Break out conversion into script Modify sketch and edge detection steps to get a cleaner image

--- sketch_ws.py
+++ sketch_ws.py
@@ -1,12 +1,15 @@
 #! /usr/bin/env python
 from subprocess import call
-from bottle import route, run, static_file, request
+from bottle import route, run, static_file, request, template
 import os, glob
 
-BASEDIR = '/usr/local/SketchWS'
+try:
+  os.makedirs(sketches)
+except:
+  pass
 
 images = {}
-for f in glob.glob("%s/*/base.*" % BASEDIR):
+for f in glob.glob("sketches/*/base.*"):
   name, ext = os.path.splitext(f)
   base = os.path.basename(os.path.dirname(name))
   images[base] = ext
@@ -21,33 +24,24 @@ def static(filename):
 
 @route('/sketch/<name>/<filename>')
 def static(name, filename):
-    return static_file(filename, root="%s/%s" % (BASEDIR, name))
+    return static_file(filename, root="sketches/%s" % (name))
 
 @route('/upload', method='POST')
 def upload():
     img = request.files.get('img')
     name, ext = os.path.splitext(img.filename)
-    path = "%s/%s" % (BASEDIR, name)
+    path = "sketches/%s" % (name)
     try:
       os.makedirs(path)
     except:
       pass
     img.save("%s/base%s" % (path, ext), overwrite=True)
-    convert_to_sketch(path, ext)
+    call("./convert_to_sketch %s %s" % (name, ext), shell=True)
     images[name] = ext
     return images
 
-@route('/print')
-def print_it():
-    pass
-
-def convert_to_sketch(path, ext):
-  call("convert -alpha off %s/base%s -set colorspace Gray -separate -average %s/0.png" % (path, ext, path), shell=True)
-  call("convert %s/0.png -edge 3 -normalize -negate %s/e.png" % (path, path), shell=True)
-  call("convert %s/0.png -sketch 0x20+120 %s/s.png" % (path, path), shell=True)
-  call("composite -compose darken %s/e.png %s/s.png %s/1.pgm" % (path, path, path), shell=True)
-  call("potrace -t 2 -p %s/1.pgm" % path, shell=True)
-  call("pstoedit -f gcode %s/1.ps %s/sketch.gcode" % (path, path), shell=True)
-  call("convert -resize 128x128 %s/1.ps %s/thumb.png" % (path, path), shell=True)
+@route('/sketches/<name>/print')
+def print_it(sketch):
+    call("./print_sketch %s" % sketch, shell=True)
 
 run()
